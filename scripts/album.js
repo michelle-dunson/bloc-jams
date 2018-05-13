46997426ec642a3b6b1ca0214c2a54a5c13ee59b
Michelle <mdunson88@gmail.com>
2018-05-13 12:32:29 -0700

    Assignment 33 completed

--- scripts/album.js
+++ scripts/album.js
@@ -45,13 +45,14 @@ var createSongRow = function(songNumber, songName, songLength) {
              currentSoundFile.play();
              updatePlayerBarSong();
          } else if (currentlyPlayingSongNumber === songNumber) {
+             var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
              if (currentSoundFile.isPaused()) {
                  $(this).html(pauseButtonTemplate);
-                 $('.main-controls .play-pause').html(playerBarPauseButton);
+                 $playPauseButton.html(playerBarPauseButton);
                  currentSoundFile.play();
              } else {
-                 $(this).html(playButtonTemplate);
-                 $('.main-controls .play-pause').html(playerBarPlayButton);
+                 currentlyPlayingCell.html(playButtonTemplate);
+                 $playPauseButton.html(playerBarPlayButton);
                  currentSoundFile.pause();
              }
          }
@@ -153,6 +154,20 @@ var previousSong = function() {
     $lastSongNumberCell.html(lastSongNumber);
 };
 
+var togglePlayFromPlayerBar = function() {
+    if (currentSoundFile.isPaused()) {
+        var $songNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+        $songNumberCell.html(pauseButtonTemplate);
+        $playPauseButton.html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else if (currentSoundFile) {
+        var $songNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+        $songNumberCell.html(playButtonTemplate);
+        $playPauseButton.html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -164,9 +179,11 @@ var currentSoundFile = null;
 var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
 });
